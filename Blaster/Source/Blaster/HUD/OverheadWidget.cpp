070b7eb61febecb468acb214e2464b016d6770f3
bradleyb <bradleyb@sfu.ca>
2023-08-17 15:04:55 -0700

    Added base assets and enabled seamless server travel

---
+++ Blaster/Source/Blaster/HUD/OverheadWidget.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OverheadWidget.h"
+#include "Components/TextBlock.h"
+#include "GameFramework/PlayerState.h"
+
+void UOverheadWidget::SetDisplayText(FString TextToDisplay) 
+{
+	if (DisplayText)
+	{
+		DisplayText->SetText(FText::FromString(TextToDisplay));
+	}
+}
+
+void UOverheadWidget::ShowPlayerNetRole(APawn* InPawn)
+{
+	ENetRole RemoteRole = InPawn->GetRemoteRole();
+	FString Role;
+
+	switch (RemoteRole)
+	{
+	case ENetRole::ROLE_Authority:
+		Role = FString("Authority");
+		break;
+	case ENetRole::ROLE_AutonomousProxy:
+		Role = FString("Autonomous Proxy");
+		break;
+	case ENetRole::ROLE_SimulatedProxy:
+		Role = FString("Simulated Proxy");
+		break;
+	case ENetRole::ROLE_None:
+		Role = FString("None");
+		break;
+	default:
+		Role = FString("Smelly Butt Face");
+		break;
+	}
+
+	APlayerState* PlayerState = InPawn->GetPlayerState();
+	
+	FString PlayerName = "Pawn";
+	if (PlayerState)
+	{
+		PlayerName = PlayerState->GetPlayerName();
+	}
+
+	FString RemoteRoleString = FString::Printf(TEXT("%s: %s"), *PlayerName, *Role);
+	SetDisplayText(RemoteRoleString);
+}
+
+void UOverheadWidget::NativeDestruct()
+{
+	RemoveFromParent();
+	Super::NativeDestruct();
+}
